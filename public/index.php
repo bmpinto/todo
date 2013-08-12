5f4bc62aced708eba9cba2289d75104df35c0085
Bruno Pinto <bmorosopinto@gmail.com>
2013-08-12 05:03:21 +0100

    adding mvc

--- public/index.php
+++ public/index.php
@@ -45,6 +45,7 @@ $app_name = idx($app_info, 'name', '');
     <title>BMPinto - Todo App</title>
     <link rel="stylesheet" href="public/css/styles.css" />
     <link rel="stylesheet" href="public/css/styles-init.css" />
+    <script src="public/js/jquery-1.10.2.min.js"></script>
 
     <meta property="og:title" content="<?php echo he($app_name); ?>" />
     <meta property="og:type" content="website" />
@@ -110,6 +111,5 @@ $app_name = idx($app_info, 'name', '');
         $_SESSION['user_id'] = $user_id;
       }
     ?>
-    <script src="public/js/jquery-1.10.2.min.js"></script>
   </body>
 </html>
