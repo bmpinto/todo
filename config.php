71d5b636a27bea28cafede0a6693c239b661ae19
Bruno Pinto <bmorosopinto@gmail.com>
2013-08-16 14:22:52 +0100

    add env

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 		    . "host=ec2-54-221-203-200.compute-1.amazonaws.com;"
 		    . $_ENV['dbname']
 		    . $_ENV['dbuser']
-		    . "port=5432;"
+		    . ";port=5432;"
 		    . "sslmode=require;"
 		    . $_ENV['dbpw'];
 
