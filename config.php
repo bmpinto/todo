c83943a52ad4e9fdc42927600cbcb49988bdd0ca
Bruno Pinto <bmorosopinto@gmail.com>
2013-08-16 14:34:01 +0100

    add env

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 	$config = "pgsql:"
 		    . "host=ec2-54-221-203-200.compute-1.amazonaws.com;"
-		    . "dbname=d9m44lac0b8q1a;"
-		    . "user=aauaqcstenptmg;"
+		    . "dbname=". getenv("dbname") .";"
+		    . "user=". getenv("dbuser") .";"
 		    . "port=5432;"
 		    . "sslmode=require;"
-		    . "password=cyrSMITV7QfZBGQGt_JbN3G70c";
+		    . "password=". getenv("dbpw");
 
 ?>
